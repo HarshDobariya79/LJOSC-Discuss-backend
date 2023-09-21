43ce6a58c215a3a4569dfad8a65849e32c37220b
Harsh Dobariya <harshdobariya79@gmail.com>
2023-09-21 14:28:55 +0530

    Fix JWT expiration

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -2,6 +2,7 @@ const jwt = require("jsonwebtoken");
 const User = require("../models/user")
 
 const ACCESS_TOKEN_SIGN_KEY = process.env.ACCESS_TOKEN_SIGN_KEY;
+const ACCESS_TOKEN_CLOCK_TOLERANCE = 5000; // 5 seconds
 
 const authenticate = async (req, res, next) => {
   const token = req.header("Authorization");
@@ -11,7 +12,12 @@ const authenticate = async (req, res, next) => {
   }
 
   try {
-    const decoded = jwt.verify(token, ACCESS_TOKEN_SIGN_KEY, {clockTolerance: "10s"});
+    const decoded = jwt.verify(token, ACCESS_TOKEN_SIGN_KEY);
+
+    if (decoded.exp <= Date.now() / ACCESS_TOKEN_CLOCK_TOLERANCE) {
+      return res.status(401).json({ error: 'Access token has expired' });
+    }
+
     req.user = await User.findOne({_id: decoded?.userId});
     if (!req.user) {
       res.status(404).send({ message: "User not found" });
