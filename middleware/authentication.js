53def82a6149070540fd68888f80b2414d00ad9b
Harsh Dobariya <harshdobariya79@gmail.com>
2023-09-22 20:29:57 +0530

    Implement Bearer scheme in JWT auth

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -1,24 +1,26 @@
 const jwt = require("jsonwebtoken");
-const User = require("../models/user")
+const User = require("../models/user");
 
 const ACCESS_TOKEN_SIGN_KEY = process.env.ACCESS_TOKEN_SIGN_KEY;
 const ACCESS_TOKEN_CLOCK_TOLERANCE = 5000; // 5 seconds
 
 const authenticate = async (req, res, next) => {
-  const token = req.header("Authorization");
-
-  if (!token) {
-    return res.status(401).json({ error: "Authentication failed" });
-  }
-
   try {
+    const authHeader = req.header("Authorization");
+    const type = authHeader.split(" ")[0];
+    const token = authHeader.split(" ")[1];
+
+    if (!token || type !== "Bearer") {
+      return res.status(401).json({ error: "Authentication failed" });
+    }
+
     const decoded = jwt.verify(token, ACCESS_TOKEN_SIGN_KEY);
 
     if (decoded.exp <= Date.now() / ACCESS_TOKEN_CLOCK_TOLERANCE) {
-      return res.status(401).json({ error: 'Access token has expired' });
+      return res.status(401).json({ error: "Access token has expired" });
     }
 
-    req.user = await User.findOne({_id: decoded?.userId});
+    req.user = await User.findOne({ _id: decoded?.userId });
     if (!req.user) {
       res.status(404).send({ message: "User not found" });
     } else {
