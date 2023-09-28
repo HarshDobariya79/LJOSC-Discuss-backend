554ed7c264932cc1871b170c7081ab5cc879ed33
Harsh Dobariya <harshdobariya79@gmail.com>
2023-09-28 15:05:54 +0530

    Add index on createDate inside thread schema

--- models/thread.js
+++ models/thread.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const threadSchema = new mongoose.Schema({
   title: {type: String, required: true},
   content: {type: String, required: true},
-  createDate: { type: Date, default: Date.now },
+  createDate: { type: Date, default: Date.now, index: true},
   author: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
   views: {
     type: Map,
